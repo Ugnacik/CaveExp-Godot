9d98e5c24f6d2f60ddab607ec180b0e40c1ee487
Ugnacik <92326804+Ugnacik@users.noreply.github.com>
2026-04-04 21:01:46 +0200

    Changed room tile positions
    The tiles now go down from (0,0) instead of up

--- Scripts/LevelGeneration/Room.cs
+++ Scripts/LevelGeneration/Room.cs
@@ -8,7 +8,7 @@ public partial class Room : Node2D
     [Export] public bool OpeningTop = false;
     [Export] public bool OpeningBottom = false;
 
-    public override void _Draw()
+    /*public override void _Draw()
     {
         Color color = Colors.White;
 
@@ -17,6 +17,6 @@ public partial class Room : Node2D
         if (OpeningTop) color = Colors.Blue;
         if (OpeningBottom) color = Colors.Yellow;
 
-        DrawRect(new Rect2(Vector2.Zero, new Vector2(192, 192)), color, false);
-    }
+        DrawRect(new Rect2(Vector2.Zero, new Vector2(192, 128)), color, true, 1, false);
+    }*/
 }
